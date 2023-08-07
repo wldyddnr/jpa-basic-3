6dc63c7824c28cd38714d6d94cbf3c398e462f82
yonguk <wldyddnr@gmail.com>
2023-08-07 17:29:50 +0900

    Named쿼리

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -15,26 +15,46 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
         try {
-            Team team = new Team();
-            team.setName("team");
-            em.persist(team);
+            Team team1 = new Team();
+            team1.setName("teamA");
+            Team team2 = new Team();
+            team2.setName("teamB");
+            Team team3 = new Team();
+            team3.setName("teamC");
+            em.persist(team1);
+            em.persist(team2);
+            em.persist(team3);
 
-            Member member = new Member();
-            member.setUsername("member");
-            member.setAge(10);
-            member.setTeam(team);
+            Member member1 = new Member();
+            member1.setUsername("회원1");
+            member1.setTeam(team1);
 
-            em.persist(member);
+            Member member2 = new Member();
+            member2.setUsername("회원2");
+            member2.setTeam(team1);
+
+            Member member3 = new Member();
+            member3.setUsername("회원3");
+            member3.setTeam(team2);
+
+            Member member4 = new Member();
+            member4.setUsername("회원4");
+
+            em.persist(member1);
+            em.persist(member2);
+            em.persist(member3);
+            em.persist(member4);
 
 
             em.flush();
             em.clear();
 
-            String query = "select t.members from Team t ";
-            Collection resultList = em.createQuery(query, Collection.class)
-                    .getResultList();
-            for (Object s : resultList) {
-                System.out.println("s = " + s);
+            List<Member> resultList1 = em.createNamedQuery("Member.findByUsername", Member.class).
+                    setParameter("username", "회원1").
+                    getResultList();
+
+            for (Member member : resultList1) {
+                System.out.println("member = " + member);
             }
 
 
